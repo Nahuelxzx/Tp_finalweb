7bc11958148661d79fdd87b631d43e0ead8250c6
nahuelxzx <loabzurdo2@gmail.com>
2014-07-07 02:26:32 -0300

    Cambios en la BAse
    Cambios en la base pasaje vuelo

--- Base_Datos/tp_finalweb2_avion.sql
+++ Base_Datos/tp_finalweb2_avion.sql
@@ -4,7 +4,7 @@ USE `tp_finalweb2`;
 --
 -- Host: localhost    Database: tp_finalweb2
 -- ------------------------------------------------------
--- Server version	5.6.14-enterprise-commercial-advanced
+-- Server version	5.6.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -58,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-07-01 15:49:12
+-- Dump completed on 2014-07-07  2:25:27
