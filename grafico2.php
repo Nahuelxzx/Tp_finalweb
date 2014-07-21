1fddba1a114785db6a9d666d887a40f07af805c1
nahuelxzx <loabzurdo2@gmail.com>
2014-07-20 21:14:30 -0300

    DesdeCasa
    Comit por las dudas

--- grafico2.php
+++ grafico2.php
@@ -1,31 +1,43 @@
 <?php
-
+ //include ('Conexion/estructuraConsulta.php');
  require_once ('jpgraph/src/jpgraph.php'); 
  require_once ('jpgraph/src/jpgraph_pie.php');
  require_once ('jpgraph/src/jpgraph_pie3d.php');
 
- require_once ('Conexion/estructuraConsulta.php');
+ 
 
- $estructuraConsulta = new estructuraModelo();
+ /*$estructuraConsulta = new estructuraModelo();
 
  $diasvuelos = $estructuraConsulta->get_sql("select count(*) as ventas from pasaje where habilitado='si' ");
-
-/* foreach ($diasvuelos as $row){	
+ //echo $diasvuelos["ventas"];
+// printf("%s",$diasvuelos["ventas"]);
+//var_dump($diasvuelos);
+ foreach ($diasvuelos as $row){	
  	$num = $row['ventas'];
+ }
+
+ //echo "$num";
+
+$diasvuelos2 = $estructuraConsulta->get_sql("select count(*) as ventas from pasaje where habilitado='no' ");
+ //echo $diasvuelos["ventas"];
+// printf("%s",$diasvuelos["ventas"]);
+//var_dump($diasvuelos2);
+ foreach ($diasvuelos2 as $row2){	
+ 	$num2 = $row2['ventas'];
  }*/
 
- while($row = mysql_fetch_array($diasvuelos))
+ /*while($row = mysql_fetch_array($diasvuelos))
     {
-     $data[] = $row[0];
+     $data[] = $row['ventas'];
      //$can[] = $row[1];
-    } 
+    } */
  //var_dump($diasvuelos);
 
 //$grafico->xaxis->SetTickLabels($labes);  remplazar por:  $grafico->xaxis->SetTickLabels($usuarios);
-//$datos =array($num);
-//$datos = array(6,5,8,6);
+//$datos =array($num,$num2);
+$datos = array(1,2);
 //$labels = array("pepe","juanita","Maria","Luis");
-$labels = array("pepe");
+$labels = array("pepe","rosa");
 $grafico = new PieGraph(500, 400, 'auto');
 $grafico->img->SetAntiAliasing();
 $grafico->SetMarginColor("gray");
